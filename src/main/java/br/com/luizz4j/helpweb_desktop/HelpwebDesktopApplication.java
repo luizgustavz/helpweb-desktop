5c553b9f6162590cdd33368015e3a24dc328a976
luizgustavz <luiz.gus.ms@gmail.com>
2025-10-06 19:33:59 -0300

    first commit

---
+++ src/main/java/br/com/luizz4j/helpweb_desktop/HelpwebDesktopApplication.java
@@ -0,0 +1,13 @@
+package br.com.luizz4j.helpweb_desktop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpwebDesktopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpwebDesktopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/luizz4j/helpweb_desktop/HelpwebDesktopApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luizz4j.helpweb_desktop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpwebDesktopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
